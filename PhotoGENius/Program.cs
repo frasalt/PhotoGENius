c2a01a348019dc494c8d06cbe73755e20ea4e5c3
Martino <martino.zanetti@gmail.com>
2022-03-09 12:26:12 +0100

    Updated program main

--- PhotoGENius/Program.cs
+++ PhotoGENius/Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+//using namespace PGENLib;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
