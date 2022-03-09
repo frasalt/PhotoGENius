c06170fdb1aa4374aa6aceafa3e57887dd5a47d9
Teresa Lamorte <teresa.lamorte@studenti.unimi.it>
2022-03-09 12:32:53 +0100

    Define color class

--- PGENLib/Color.cs
+++ PGENLib/Color.cs
@@ -1,5 +1,17 @@
-﻿namespace PGENLib;
-public class Color
+﻿namespace PGENLib
 {
+    public struct Color
+    { 
 
+        public Color(double r, double g, double b) //Costruttore
+        {
+            this.r = 0;
+            this.g = 0;
+            this.b = 0;
+        }
+        public double r { get; }
+        public double g { get; }
+        public double b { get; }
+        public override string ToString() => $"({r}, {g}, {b})";
+    }
 }
