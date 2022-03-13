f18324600e640cc4b76f8aea0b656c30000667f4
Francesca Salteri <franci.salteri@gmail.com>
2022-03-13 18:13:16 +0100

    Overloading operator+

--- PGENLib/Color.cs
+++ PGENLib/Color.cs
@@ -5,27 +5,41 @@ namespace PGENLib
 {
     public struct Color
     {
-        public float r;
-        public float g;
-        public float b;
+        private float r;
+        private float g;
+        private float b;
 
-        public Color(float r, float g, float b) //Costruttore
+        //Costruttori---------------------------------
+        public Color()
+        {
+            this.r = 0;
+            this.g = 0;
+            this.b = 0;
+        }
+
+        public Color(float r, float g, float b)
         {
             this.r = r;
             this.g = g;
             this.b = b;
         }
 
+        //Metodi-------------------------------------
         public override string ToString()
         {
             return $"({r}, {g}, {b})";
         }
+
+        public static Color operator +(Color c1, Color c2)
+        {
+            Color c = new Color();
+            c.r = c1.r + c2.r;
+            c.g = c1.g + c2.g;
+            c.b = c1.b + c2.b;
+            return c;
+        }
+        
+
     };
 
-    //Somma 
-    //public static add operator +(add c1, add c2)
-    //{
-    //return new add(c1.r + c2.r, c1.g + c2.g, c1.b + c2.b);
-    //};
-
-}
+    
