6025eec5b7f3a81499fac235db6af0d9872a5ff4
Martino <martino.zanetti@gmail.com>
2022-03-30 12:13:14 +0200

    cleaned main

--- PhotoGENius/Program.cs
+++ PhotoGENius/Program.cs
@@ -49,56 +49,34 @@ class Program
    //----------------------------------------------------------------------------------------------------------- 
     static void Main(string[] argv)
     {
+        
         /*
-        // prova lettura da stream
-        HdrImage img2 = new HdrImage(3, 2);
-
-        var col = new Color((float)1.0e1, (float)2.0e1, (float)3.0e1);
-        img2.SetPixel(0, 0, col);
-        col = new Color((float)4.0e1, (float)5.0e1, (float)6.0e1);
-        img2.SetPixel(1, 0, col);
-        col = new Color((float) 7.0e1, (float) 8.0e1, (float) 9.0e1);
-        img2.SetPixel(2, 0, col);
-        col = new Color((float)1.0e2, (float)2.0e2, (float)3.0e2);
-        img2.SetPixel(0, 1, col);
-        col = new Color((float)4.0e2, (float)5.0e2, (float)6.0e2);
-        img2.SetPixel(1, 1, col);
-        col = new Color((float)7.0e2, (float)8.0e2, (float)9.0e2);
-        img2.SetPixel(2, 1, col);
-
-        Stream buf = new MemoryStream();
-        img2.WritePFMFile(buf, Endianness.BigEndian);
-        */
-        
-        
         var img = new HdrImage(3, 2);
         
-        // prova con BigEndianness
-        HdrImage prova_be;
+        // prova con Endianness
+        HdrImage prova;
         
         using (Stream fileStream = File.OpenRead(@"..\..\..\..\PGENLib.tests\reference_be.pfm"))
-        { prova_be = img.ReadPFMFile(fileStream); }
+        { prova = img.ReadPFMFile(fileStream); }
 
         using (Stream outFileStream = File.OpenWrite("file_BB.pfm"))
-        { prova_be.WritePFMFile(outFileStream, Endianness.BigEndian); }
+        { prova.WritePFMFile(outFileStream, Endianness.BigEndian); }
         
         using (Stream outFileStream2 = File.OpenWrite("file_BL.pfm"))
-        { prova_be.WritePFMFile(outFileStream2, Endianness.LittleEndian); }
-        
-        // prova con BigEndianness
-        HdrImage prova_le;
+        { prova.WritePFMFile(outFileStream2, Endianness.LittleEndian); }
         
         using (Stream fileStream = File.OpenRead(@"..\..\..\..\PGENLib.tests\reference_le.pfm"))
-        { prova_be = img.ReadPFMFile(fileStream); }
+        { prova = img.ReadPFMFile(fileStream); }
 
         using (Stream outFileStream = File.OpenWrite("file_LB.pfm"))
-        { prova_be.WritePFMFile(outFileStream, Endianness.BigEndian); }
+        { prova.WritePFMFile(outFileStream, Endianness.BigEndian); }
         
         using (Stream outFileStream2 = File.OpenWrite("file_LL.pfm"))
-        { prova_be.WritePFMFile(outFileStream2, Endianness.LittleEndian); }
+        { prova.WritePFMFile(outFileStream2, Endianness.LittleEndian); }
         //*/
 
-        /* // main definitivo, una volta che funzionano readpfm e writepfm
+        /*
+        // main definitivo, una volta che funzionano readpfm e writepfm
         Parameters parameters = null;
         try
         {
@@ -129,6 +107,6 @@ class Program
         }
 
         Console.WriteLine($"File {parameters.OutputPngFileName} has been written to disk.");
-        */
+        //*/
     }
 }
