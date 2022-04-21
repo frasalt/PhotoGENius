1b14908fa03c81613fe60dd766ec7a4b16cc2ca7
Francesca Salteri <franci.salteri@gmail.com>
2022-04-21 12:46:00 +0200

    OrthogolanCamera and Perspective camera implemented with tests

--- PGENLib.Tests/CamerasTests.cs
+++ PGENLib.Tests/CamerasTests.cs
@@ -8,21 +8,75 @@ namespace PGENLib.Tests
     public class CamerasTest
     {
         [Fact]
-        public void test_isclose()
+        public void test_areclose()
         {
             Ray a = new Ray(new Point(1.0f, 2.0f, 3.0f), new Vec(5.0f, 4.0f, -1.0f));
             Ray b = new Ray(new Point(1.0f, 2.0f, 3.0f), new Vec(5.0f, 4.0f, -1.0f));
             Ray c = new Ray(new Point(5.0f, 1.0f, 4.0f), new Vec(3.0f, 9.0f, 4.0f));
-            Assert.True(Ray.is_close(a, b));
+            Assert.True(Ray.are_close(a, b));
         }
   
         [Fact]
         public void test_at()
         {
             Ray d = new Ray(new Point(1.0f, 2.0f, 4.0f), new Vec(4.0f, 2.0f, 1.0f)); 
-            Assert.True(Point.are_close(Ray.at(d, 0.0f), d.Origin));
-            Assert.True(Point.are_close(Ray.at(d, 1.0f), new Point(5.0f, 4.0f, 5.0f)));
-            Assert.True(Point.are_close(Ray.at(d, 2.0f), new Point(9.0f, 6.0f, 6.0f)));
+            Assert.True(Point.are_close(d.At(0.0f), d.Origin));
+            Assert.True(Point.are_close(d.At(1.0f), new Point(5.0f, 4.0f, 5.0f)));
+            Assert.True(Point.are_close(d.At(2.0f), new Point(9.0f, 6.0f, 6.0f)));
         }
+        
+        [Fact]
+        public void test_ortCamera()
+        {
+            var cam = new OrthogonalCamera(2.0f);
+            var ray1 = cam.FireRay(0.0f, 0.0f);
+            var ray2 = cam.FireRay(1.0f, 0.0f);
+            var ray3 = cam.FireRay(0.0f, 1.0f);
+            var ray4 = cam.FireRay(1.0f, 1.0f);
+            
+            //Are rays parallel (as they should be)?
+            //Assert.True(Math.Abs(Vec.SquaredNorm(Vec.CrossProduct(ray1.Dir, ray2.Dir)), 0.0f)<1E-5);
+            Assert.True(Vec.SquaredNorm(Vec.CrossProduct(ray1.Dir, ray2.Dir)) == 0.0f);
+            Assert.True(Vec.SquaredNorm(Vec.CrossProduct(ray1.Dir, ray3.Dir)) == 0.0f);
+            Assert.True(Vec.SquaredNorm(Vec.CrossProduct(ray1.Dir, ray4.Dir)) == 0.0f);
+            
+            //Right coordinates?
+            Assert.True(Point.are_close(ray1.At(1.0f), new Point(0.0f, 2.0f, -1.0f)));
+            Assert.True(Point.are_close(ray2.At(1.0f), new Point(0.0f, -2.0f, -1.0f)));
+            Assert.True(Point.are_close(ray3.At(1.0f), new Point(0.0f, 2.0f, 1.0f)));
+            Assert.True(Point.are_close(ray4.At(1.0f), new Point(0.0f, -2.0f, 1.0f)));
+        }
+
+        [Fact]
+        public void test_ortCameraTransform()
+        {
+            var Vy = new Vec(0.0f, 1.0f, 0.0f);
+            var tr = Transformation.Traslation(-Vy * 2.0f)*Transformation.RotationZ(90);
+            var cam = new OrthogonalCamera(2.0f, tr);
+            var Ray = cam.FireRay(0.5f, 0.5f);
+            Assert.True(Point.are_close(Ray.At(1.0f), new Point(0.0f, -2.0f, 0.0f)));
+        }
+        
+        [Fact]
+        public void test_PerspCamera()
+        {
+            var cam = new PerspectiveCamera(1.0f, 2.0f);
+            var ray1 = cam.FireRay(0.0f, 0.0f);
+            var ray2 = cam.FireRay(1.0f, 0.0f);
+            var ray3 = cam.FireRay(0.0f, 1.0f);
+            var ray4 = cam.FireRay(1.0f, 1.0f);
+            
+            //Do all the rays have he same origin?
+            Assert.True(Point.are_close(ray1.Origin, ray2.Origin));
+            Assert.True(Point.are_close(ray1.Origin, ray3.Origin));
+            Assert.True(Point.are_close(ray1.Origin, ray4.Origin));
+            
+            //Right coordinates?
+            Assert.True(Point.are_close(ray1.At(1.0f), new Point(0.0f, 2.0f, -1.0f)));
+            Assert.True(Point.are_close(ray2.At(1.0f), new Point(0.0f, -2.0f, -1.0f)));
+            Assert.True(Point.are_close(ray3.At(1.0f), new Point(0.0f, 2.0f, 1.0f)));
+            Assert.True(Point.are_close(ray4.At(1.0f), new Point(0.0f, -2.0f, 1.0f)));
+        }
+        
     }
 }
--- PGENLib/Cameras.cs
+++ PGENLib/Cameras.cs
@@ -1,27 +1,7 @@
 namespace PGENLib
 {
-/*
-    public interface ICamera
-    {
-        void FireRay(float u, float v);
+    
 
-    }
-
-    public struct OrthogonalCamera : ICamera
-    {
-        private float AspectRatio;
-        private Transformation tr;
-
-        public void FireRay(float u, float v)
-        {
-            Point origin = new Point(-1.0, (1.0 - 2 * u) * this.AspectRatio, 2 * v - 1);
-            direction = VEC_X;
-            return Ray(origin = origin, dir = direction, tmin = 1.0).transform(self.transformation);
-        }
-
-
-    }
-*/
     public struct Ray
     {
         public Point Origin;
@@ -37,19 +17,31 @@ namespace PGENLib
         {
             Origin = new Point(0, 0, 0);
             Dir = new Vec(0, 0, 0);
-            Tmin = 0;
-            Tmax = 0;
+            Tmin = 0.0f;
+            Tmax = 0.0f;
             Depth = 0;
         }
 
         /// <summary>
         /// Constructor to create a Ray.
         /// </summary>
-        public Ray(Point Origin, Vec Dir)
+        public Ray(Point origin, Vec dir)
         {
-            this.Origin = Origin;
-            this.Dir = Dir;
+            this.Origin = origin;
+            this.Dir = dir;
             this.Tmin = 1e-5f;
+            this.Tmax = float.PositiveInfinity;
+            this.Depth = 0;
+        }
+        
+        /// <summary>
+        /// Constructor to create a Ray.
+        /// </summary>
+        public Ray(Point origin, Vec dir, float tmin)
+        {
+            this.Origin = origin;
+            this.Dir = dir;
+            this.Tmin = tmin;
             this.Tmax = float.PositiveInfinity;
             this.Depth = 0;
         }
@@ -64,7 +56,7 @@ namespace PGENLib
             return Dir;
         }
 
-        public static bool is_close(Ray a, Ray b)
+        public static bool are_close(Ray a, Ray b)
         {
             if (Point.are_close(a.Origin, b.Origin) & Vec.are_close(a.Dir, b.Dir))
             {
@@ -76,10 +68,96 @@ namespace PGENLib
             }
         }
 
-        public static Point at(Ray a, float t)
+        public Point At(float t)
         {
-            return a.Origin + (a.Dir * t);
+            return this.Origin + this.Dir * t;
+        }
 
+        public Ray Transform(Transformation tr)
+        {
+            var newRay = new Ray(tr*this.Origin, tr*this.Dir)
+            {
+                Tmax = this.Tmax,
+                Tmin = this.Tmin,
+                Depth = this.Depth
+            };
+            return newRay; 
+        }
+        
+    }
+    
+    //==============================================================================================================
+    //Cameras
+    //==============================================================================================================
+    /// <summary>
+    /// Interface for camera: the method FireRay will be implemented in two different ways by OrthogonalCamera and
+    /// PerspectiveCamera.
+    /// </summary>
+    public interface ICamera
+    {
+        Ray FireRay(float u, float v);
+
+    }
+
+    public struct OrthogonalCamera : ICamera
+    {
+        private float AspectRatio;
+        private Transformation Transf;
+
+        public OrthogonalCamera(float aspectRatio)
+        {
+            AspectRatio = aspectRatio;
+            Transf = new Transformation();
+        }
+        public OrthogonalCamera(float aspectRatio, Transformation tr)
+        {
+            AspectRatio = aspectRatio;
+            Transf = tr;
+        }
+
+        public Ray FireRay(float u, float v)
+        {
+            var origin = new Point(-1.0f, (1.0f - 2.0f * u) * this.AspectRatio, 2.0f * v - 1.0f);
+            var direction = new Vec(1.0f, 0.0f, 0.0f);
+            var rayToFire = new Ray(origin, direction);
+                
+            return rayToFire.Transform(this.Transf);
         }
     }
+    
+    public struct PerspectiveCamera : ICamera
+    {
+        private float ScreenDistance;
+        private float AspectRatio;
+        private Transformation Transf;
+        
+        public PerspectiveCamera()
+        {
+            ScreenDistance = 1.0f;
+            AspectRatio = 1.0f;
+            Transf = new Transformation();
+        }
+        public PerspectiveCamera(float distance, float aspectRatio)
+        {
+            ScreenDistance = distance;
+            AspectRatio = aspectRatio;
+            Transf = new Transformation();
+        }
+        public PerspectiveCamera(float distance, float aspectRatio, Transformation tr)
+        {
+            ScreenDistance = distance;
+            AspectRatio = aspectRatio;
+            Transf = tr;
+        }
+
+        public Ray FireRay(float u, float v)
+        {
+            var origin = new Point(-this.ScreenDistance, 0.0f, 0.0f);
+            var direction = new Vec(this.ScreenDistance, (1.0f - 2.0f * u) * this.AspectRatio, 2.0f * v - 1.0f);
+            var rayToFire = new Ray(origin, direction, 1.0f);
+                
+            return rayToFire.Transform(this.Transf);
+        }
+    }
+    
 }
