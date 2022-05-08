62e0bc3957ea72063590d95f1905847a40e7ddd8
Teresa Lamorte <teresa.lamorte@studenti.unimi.it>
2022-05-08 17:28:26 +0200

    Fix ShapeTests.cs

--- PGENLib.Tests/ShapeTests.cs
+++ PGENLib.Tests/ShapeTests.cs
@@ -2,27 +2,64 @@ using Xunit;
 using System;
 using System.Net.Sockets;
 using System.Numerics;
-using System;
+using System.Runtime.Intrinsics;
 using PGENLib;
 
 namespace PGENLib.Tests
 {
-    public class ShapeTest
+    public class ShapeTests
     {
-        private static Vec vecZ = new Vec(0.0f, 0.0f, 1.0f);
-        private static Sphere sphere = new Sphere();
-        static Ray ray1 = new Ray(origin: new Point(0f, 0f, 2f), dir: -vecZ);
-        HitRecord? intersection1 = sphere.RayIntersection(ray1);
-        Assert.True(intersection1 != null);
-        HitRecord hit1 = new HitRecord(
-            new Point(0.0f, 0.0f, 1.0f),
-            new Normal(0.0f, 0.0f, 1.0f),
-            new Vec2d(0.0f, 0.0f),
-            1.0f,
-            ray1
-        );
-        Assert.True(hit1.are_close(intersection1));
+        private readonly Vec Vx = new Vec(1.0f, 0.0f, 0.0f);
+        private readonly Vec Vz = new Vec(0.0f, 0.0f, 1.0f);
+        [Fact]
+        public void TestHitSphere()
+        {
+            Sphere sphere = new Sphere();
+            Ray ray1 = new Ray(origin: new Point(0f, 0f, 2f), dir: -Vz);
+            HitRecord? int1 = sphere.RayIntersection(ray1);
+            Assert.True(int1 != null);
+            HitRecord hit1 = new HitRecord(
+                new Point(0.0f, 0.0f, 1.0f),
+                new Normal(0.0f, 0.0f, 1.0f),
+                new Vec2d(0.0f, 0.0f),
+                1.0f,
+                ray1
+            );
+            Assert.True(HitRecord.are_close(hit1,int1.Value));
+
+            Ray ray2 = new Ray(new Point(3f, 0f, 0f), -Vx);
+            HitRecord? int2 = sphere.RayIntersection(ray2);
+            Assert.True(int2 != null);
+            HitRecord hit2 = new HitRecord(
+                new Point(1.0f, 0.0f, 0.0f),
+                new Normal(1.0f, 0.0f, 0.0f),
+                new Vec2d(0.0f, 0.5f),
+                2.0f,
+                ray2
+            );
+            Assert.True(HitRecord.are_close(hit2,int2.Value));
+
+            Assert.True(sphere.RayIntersection(new Ray(new Point(0f, 10f, 2f), -Vz)) == null);
+            
+        }
+        [Fact]
+        public void TestHitSphereIn()
+        {
+            Sphere sphere = new Sphere();
+            Ray ray = new Ray(origin: new Point(0f, 0f, 0f), dir: Vx);
+            HitRecord? int3 = sphere.RayIntersection(ray);
+            Assert.True(int3 != null);
+
+            HitRecord hit = new HitRecord(
+                new Point(1.0f, 0.0f, 0.0f),
+                new Normal(-1.0f, 0.0f, 0.0f),
+                new Vec2d(0.0f, 0.5f),
+                1.0f,
+                ray
+            );
+            Assert.True(HitRecord.are_close(hit, int3.Value));
+        }
         
     }
-    
 }
+
--- PGENLib/HitRecord.cs
+++ PGENLib/HitRecord.cs
@@ -102,5 +102,6 @@ namespace PGENLib
             }
         }
         
+        
     }
 }
