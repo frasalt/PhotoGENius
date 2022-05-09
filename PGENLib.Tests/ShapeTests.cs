3076a1b57f6e703b961a2180fb30b7d5119131ea
Teresa Lamorte <teresa.lamorte@studenti.unimi.it>
2022-05-09 12:46:02 +0200

    Add new tests in ShapeTests.cs

--- PGENLib.Tests/ShapeTests.cs
+++ PGENLib.Tests/ShapeTests.cs
@@ -59,7 +59,39 @@ namespace PGENLib.Tests
             );
             Assert.True(HitRecord.are_close(hit, int3.Value));
         }
-        
+
+        [Fact]
+        public void TestTransformation()
+        {
+            Sphere sphere = new Sphere(Transformation.Traslation(new Vec(10.0f, 0.0f, 0.0f)));
+
+            Ray ray1 = new Ray(new Point(10f, 0f, 2f), -Vz);
+            HitRecord? int1 = sphere.RayIntersection(ray1);
+            Assert.True(int1 != null);
+            HitRecord hit1 = new HitRecord(
+                new Point(10.0f, 0.0f, 1.0f),
+                new Normal(0.0f, 0.0f, 1.0f),
+                new Vec2d(0.0f, 0.0f),
+                1.0f,
+                ray1
+            );
+            Assert.True(HitRecord.are_close(hit1, int1.Value));
+
+            Ray ray2 = new Ray(new Point(13f, 0f, 0f), -Vx);
+            HitRecord? int2 = sphere.RayIntersection(ray2);
+            Assert.True(int2 != null);
+            HitRecord hit2 = new HitRecord(
+                new Point(11.0f, 0.0f, 0.0f),
+                new Normal(1.0f, 0.0f, 0.0f),
+                new Vec2d(0.0f, 0.5f),
+                2.0f,
+                ray2
+            );
+            Assert.True(HitRecord.are_close(hit2, int2.Value));
+
+            // Check if the sphere translation failed
+            Assert.True(sphere.RayIntersection(new Ray(new Point(0f, 0f, 2f), -Vz)) == null);
+            Assert.True(sphere.RayIntersection(new Ray(new Point(-10f, 0f, 0f), -Vz)) == null);
+        }
     }
 }
-
