b518d9f8fe5ebf8e647f2ebee7b256b50e1ccf4b
Martino <martino.zanetti@gmail.com>
2022-05-11 13:05:03 +0200

    ordine script

--- PhotoGENius/generate-video.sh
+++ PhotoGENius/generate-video.sh
@@ -2,4 +2,4 @@
      # -r 25: Number of frames per second
      start ffmpeg -y -r 25 -f image2 -s 640x480 -i imgs/\img%03d.png \
          -vcodec libx264 -pix_fmt yuv420p \
-         imgs/\spheres-perspective.mp4
+         imgs/\animation.mp4
