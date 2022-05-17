dbc4f00975b5254b3877cd9643812e752d20d588
martinozanetti <martino.zanetti@gmail.com>
2022-05-17 14:54:20 +0200

    try and succeed parallel computing

---
+++ PhotoGENius/generate-frame.sh
@@ -0,0 +1,5 @@
+# Angle with three digits, e.g. angle="1" → angleNNN="001"
+angleNNN=$(printf "%03d" $1)
+dotnet run PhotoGENius --width 640 --height 480 --angle-deg $1 --camera-type perspective --png-output imgs/\img$angleNNN.png
+
+     
