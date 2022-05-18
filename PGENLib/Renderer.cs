0400bbc2c19adbe0636e0efab507046beb5ffd7a
Francesca Salteri <franci.salteri@gmail.com>
2022-05-18 17:24:48 +0200

    class Pathtracer implemented

--- PGENLib/Renderer.cs
+++ PGENLib/Renderer.cs
@@ -67,4 +67,77 @@ namespace PGENLib
             return tot + tot2;
         }
     }
+
+    public class PathTracer : Renderer
+    {
+        public PCG Pcg;
+        public int NumbOfRays;
+        public int MaxDepth;
+        public int RussianRouletteLimit;
+        
+        public PathTracer(World world, PCG pcg, int numbOfRays = 10, int maxDepth = 2, int russianRouletteLimit = 3,
+            Color backgroundColor = default)
+        {
+            World = world;
+            BackgroundColor = backgroundColor;
+            Pcg = pcg;
+            NumbOfRays = numbOfRays;
+            MaxDepth = maxDepth;
+            RussianRouletteLimit = russianRouletteLimit;
+        }
+
+        public override Color Call(Ray ray)
+        {
+            if (ray.Depth > MaxDepth)
+            {
+                return new Color(0.0f, 0.0f, 0.0f);
+            }
+
+            var hitRecord = World.RayIntersection(ray);
+            if (hitRecord == null)
+            {
+                return BackgroundColor;
+            }
+
+            var hitMaterial = hitRecord.Value.Material;
+            var hitColor = hitMaterial.Brdf.Pigment.GetColor(hitRecord.Value.SurfacePoint);
+            var emittedRadiance = hitMaterial.EmittedRadiance.GetColor(hitRecord.Value.SurfacePoint);
+            var m1 = Math.Max(hitColor.r, hitColor.g);
+            var hitColorLum = Math.Max(m1, hitColor.b);
+            
+            //Russian roulette
+            if (ray.Depth >= RussianRouletteLimit)
+            {
+                var q = (float)Math.Max(0.05, 1 - hitColorLum);
+                if (Pcg.RandomFloat() > q)
+                {
+                    //Keep the recursion going, but compensate for other potentially discarded rays
+
+                    hitColor *= 1.0f / (1.0f - q);
+                }
+                else
+                {
+                    //Terminate prematurely
+                    return emittedRadiance;
+                }
+                
+            }
+            
+            //Monte Carlo integration
+            var cumRadiance = new Color(); //Black
+            if (hitColorLum > 0.0f)
+            {
+                for (var rayIndex = 0; rayIndex<NumbOfRays; rayIndex++)
+                {
+                    var newRay = hitMaterial.Brdf.ScatterRay(Pcg, hitRecord.Value.Ray.Dir, hitRecord.Value.WorldPoint,
+                        hitRecord.Value.Normal, ray.Depth+1);
+                    var newRadiance = Call(newRay); //Recursive call
+                    cumRadiance += hitColor * newRadiance;
+                }
+            }
+
+            return emittedRadiance + cumRadiance * (1.0f / NumbOfRays);
+        }
+    }
+    
 }
