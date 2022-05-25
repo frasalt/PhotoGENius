99df3158f36fb28bad513b6605fbc373bc81789c
Teresa Lamorte <teresa.lamorte@studenti.unimi.it>
2022-05-25 11:01:05 +0200

    add SouceLocation

--- PGENLib/SceneFiles.cs
+++ PGENLib/SceneFiles.cs
@@ -2,5 +2,13 @@ namespace PGENLib;
 
 public class SceneFiles
 {
-    
+   public class SourceLocation
+   {
+      private string FileName;
+      
+   } 
+   class SourceLocation:
+      file_name: str = ""
+   line_num: int = 0
+   col_num: int = 0
 }
