f5017e194c14079607f298924c6c754b72ad27da
Martino Zanetti <98329317+martinozanetti@users.noreply.github.com>
2022-06-19 09:58:45 +0200

    Add files via upload

--- BashScripts/generate-video.sh
+++ BashScripts/generate-video.sh
@@ -1,10 +1,10 @@
 # Note: doesn't need to cycle over image number, it's automatic
 
-ffmpeg -y '# overwrite video' \
-      -r 15 '# frames per second' \
+ffmpeg -y \
+      -r 25 \
       -f image2 \
-      -s 300x250 '# image width and hight' \
-      -i ../Media/imgs_png/\img%03d.png '# input files: %03d reads a 3digit number (ascending order)' \
+      -s 1080x820 \
+      -i ../Media/imgs_png/\img%03d.png \
       -vcodec libx264 -pix_fmt yuv420p \
-      ../Media/video/\animation.mp4 '# output path'
+      ../Media/videos/\animation.mp4 
  
